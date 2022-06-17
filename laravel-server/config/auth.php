400f498f93dded6e1199111f2a4194842ef5740e
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-18 00:23:16 +0300

    Including jwt facades and jwt factory inside the aliases array

--- laravel-server/config/auth.php
+++ laravel-server/config/auth.php
@@ -95,6 +95,22 @@ return [
         ],
     ],
 
+    'defaults' => [
+        'guard' => 'api',
+        'passwords' => 'users',
+    ],
+
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'hash' => false,
+        ],
+    ],
     /*
     |--------------------------------------------------------------------------
     | Password Confirmation Timeout
