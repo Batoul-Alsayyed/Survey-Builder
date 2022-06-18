7b86fbc15ae5939b7503fb4bc823a6e94093c975
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-18 15:35:13 +0300

    Calling create survey API

--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SurveyController;
 
 Route::group([
     'middleware' => 'api',
@@ -11,4 +12,10 @@ Route::group([
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+});
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'admin'
+], function ($router) {
+    Route::post('/createNewSurvey', [SurveyController::class, 'createNewSurvey']);    
 });
