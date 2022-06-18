b5a1e9fda260aa0d2e6c5c37e669a8eb520e1862
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-18 14:38:19 +0300

    Calling and testing userprofile API

--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -10,4 +10,5 @@ Route::group([
 ], function ($router) {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
 });
