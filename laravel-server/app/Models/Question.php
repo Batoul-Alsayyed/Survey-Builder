024b3ddc424a0637e1fb5481d0a6c5bf5d79823f
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-18 18:05:48 +0300

    Setting up question model

---
+++ laravel-server/app/Models/Question.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Question extends Model{
+    use HasFactory;
+    protected $fillable = [
+        'type',
+        'content',
+        'survey_id',
+    ];
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    } 
+}
