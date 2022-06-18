fa398993293ac82442def19202305d5cf36796e2
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-18 15:31:16 +0300

    Adding survey controller + creating new survey API inside it.

---
+++ laravel-server/app/Http/Controllers/SurveyController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Survey;
+use Illuminate\Http\Request;
+use Validator;
+
+class SurveyController extends Controller{
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['createNewSurvey']]);
+    }
+    public function createNewSurvey(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $survey = Survey::create(array_merge(
+                    $validator->validated(),
+                ));
+        return response()->json([
+            'message' => 'survey successfully registered',
+            'survey' => $survey
+        ], 201);
+    }
+
+}
