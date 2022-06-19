0c076f0df17c3d7c5c9c90eb0298c5b00440f26f
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-19 20:40:35 +0300

    Linking surveys with database and getting all surveys using getSurveys API

--- my-react-app/src/Survey.js
+++ my-react-app/src/Survey.js
@@ -1,23 +1,42 @@
 import React from 'react'
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 export default function Survey() {
+    const [surveys, setSurveys] = useState([]);
+    useEffect (() => {
+            axios.get("http://127.0.0.1:8000/api/surveys").then(({data})=>{
+            //console.log(data.surveys);
+            setSurveys(data.surveys);
+        });
+    });  
+
   return (
     <div>
       <h1>Survey Name</h1>
       <table>
+        <tbody>
         <tr>
             <th>Survey id</th>
             <th>Survey Name</th>
 
         </tr>
-        <tr>
-            <td>1</td>
-            <td>Survey 1</td>
-            <td><button>Add question</button></td>
-        </tr>
+
+        {surveys.map((survey)=> {
+            return (
+                <tr key={survey.id}>
+                <td>{survey.id}</td>
+                <td>{survey.name}</td>
+                <td><button>Add question</button></td>
+                </tr>
+            )
+        })}
+
+        
 
 
+        </tbody>
       </table>
 
     </div>
