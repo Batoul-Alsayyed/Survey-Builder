045a3275e1bb5b0659447b12e4b91d7457918bfd
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-20 01:11:16 +0300

    Adding signup on click handler

--- my-react-app/src/Signin.js
+++ my-react-app/src/Signin.js
@@ -1,6 +1,24 @@
 import React from 'react'
+import Register from './Register';
+import { useNavigate } from 'react-router-dom';
+import { Router } from 'react-router';
+import { BrowserRouter, Route,Routes, Link } from "react-router-dom";
+
 
 export default function Signin() {
+// let history = useHistory();
+        <Routes>
+          <Route path="/Register" element={<Register />} />
+
+        </Routes>
+  function signupOnClick(){
+    console.log("clicked!");
+    <BrowserRouter>
+    var navigate = useNavigate();
+    navigate('/Register')
+    </BrowserRouter>
+    
+  }
   return (
     <div>
       <div className='container2'>
@@ -20,10 +38,15 @@ export default function Signin() {
     <input type="text"/>
     </div>
 
-    <p>Not a member? <a href='#'>Signup</a></p>
+    <p>Not a member? 
+    <button onClick={signupOnClick}>Signup</button>
+    </p>
     <button className='RegisterBtn'>Sign-in</button>
 
     </div>
     </div>
+    
   )
+
+  
 }
