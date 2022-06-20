77c8cf8dc43c9aff35f439ace102f92d6bc248e0
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-21 01:19:33 +0300

    Adding navigation

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,14 +2,33 @@ import './App.css';
 import Register from './Register';
 import Signin from './Signin';
 import Survey from './Survey';
+import Error from './Error';
+import Home from './Home';
+import SpecificSurvey from './SpecificSurvey'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
 function App() {
   
   return (
-    <main className='App'>
-      {/* <Register/> */}
-      <Signin/> 
-      {/* <Survey/> */}
-    </main>
+    <Router>
+      <nav>
+        <Link to="/"> Homepage </Link>
+        <Link to="/Survey"> View Surveys </Link>
+        <Link to="/Signin"> Signin </Link>
+        <Link to="/Signup"> Signup </Link>
+
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/Survey' element={<Survey />}/>
+        <Route path='/Survey' element={<Survey />}/>
+        <Route path='/SpecificSurvey/:id' element={<SpecificSurvey />}/>
+        <Route path='/Signin' element={<Signin />}/>
+        <Route path='/Signup' element={<Register />}/>
+        <Route path='*' element={<Error/>}/>
+      </Routes>
+      {/*Note:  Anything that is written inside router will be constant for all pages */}
+    </Router>
   );
 }
 
